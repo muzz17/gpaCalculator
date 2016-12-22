42a7c7b167ab57864a0a587f73205ac7dcb2e7bb
Muzzammil Ahmed <Muzz@Muzzammils-MacBook-Pro.local>
2016-12-22 16:47:30 -0500

    picker is giving question marks

--- gpaCalculator/ViewController.swift
+++ gpaCalculator/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var numClassesPicker: UIPickerView!
+    
+    var numClasses = [Int]()
+    var counter: Int = 0
+    var counter2: Int = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initNumClassArray()
+        
+        self.numClassesPicker.delegate = self
+        self.numClassesPicker.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return numClasses.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    private func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> Int?{
+        return numClasses[row]
+    }
+    
+    func initNumClassArray(){
+        
+        while counter < 11 {
+            numClasses.insert(counter2, at: counter)
+            
+            counter = counter + 1
+            counter2 = counter + 1
+        }
+        
+    }
+    
+    
 }
 
