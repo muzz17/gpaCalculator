3c18dc7bda11dca93e31dd43a0e7a190de7dac99
Muzzammil Ahmed <Muzz@Muzzammils-MacBook-Pro.local>
2017-01-11 14:29:08 -0500

    add MVC organization

---
+++ gpaCalculator/aboutVC.swift
@@ -0,0 +1,19 @@
+//
+//  aboutVC.swift
+//  gpaCalculator
+//
+//  Created by Muzzammil Ahmed on 1/11/17.
+//  Copyright © 2017 Muzzammil Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class aboutVC: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+}
