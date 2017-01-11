83ca797aee27fc2b5c273f8cbef4a8146a131a3e
Muzzammil Ahmed <Muzz@Muzzammils-MacBook-Pro.local>
2017-01-11 15:22:28 -0500

    stable ... successfully added basic table view with gpa scale

--- gpaCalculator/aboutVC.swift
+++ gpaCalculator/aboutVC.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
-class aboutVC: UIViewController {
+class aboutVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // outlets
+    
+    @IBOutlet weak var gpaScaleTable: UITableView!
+    
+    // variables
+    
+    let gpaScaleArray = ["A  =  4.0", "A-  =  3.7", "B+  =  3.3", "B  =  3.0", "B-  =  2.7", "C+  =  2.3", "C  =  2.0", "C-  =  1.7", "D+  =  1.3", "D  =  1.0", "E/F  =  0.0"]
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.gpaScaleTable.delegate = self
+        self.gpaScaleTable.dataSource = self
+    }
     
+    public func tableView(_ gpaScaleTable: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gpaScaleArray.count
+    }
+    
+    public func tableView(_ gpaScaleTable: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = gpaScaleTable.dequeueReusableCell(withIdentifier: "gpaCell", for: indexPath)
+        cell.textLabel?.text = gpaScaleArray[indexPath.row]
+        return cell
+    }
+    
+    func numberOfSections(in gpaScaleTable: UITableView) -> Int {
+        return 1
+    }
     
     
     
