1f4dcb4743cac4ff307ba9d2d8662d67a19d7c9c
Muzzammil Ahmed <Muzz@Muzzammils-MacBook-Pro.local>
2017-01-11 16:59:11 -0500

    stable ... made ui improvements to about page

--- gpaCalculator/aboutVC.swift
+++ gpaCalculator/aboutVC.swift
@@ -16,7 +16,7 @@ class aboutVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // variables
     
-    let gpaScaleArray = ["A  =  4.0", "A-  =  3.7", "B+  =  3.3", "B  =  3.0", "B-  =  2.7", "C+  =  2.3", "C  =  2.0", "C-  =  1.7", "D+  =  1.3", "D  =  1.0", "E/F  =  0.0"]
+    let gpaScaleArray = ["A = 4.0", "A- = 3.7", "B+ = 3.3", "B = 3.0", "B- = 2.7", "C+ = 2.3", "C = 2.0", "C- = 1.7", "D+ = 1.3", "D = 1.0", "E/F = 0.0"]
     
     
     override func viewDidLoad() {
